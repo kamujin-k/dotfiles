6457c0498574722811dced7efc8835b9e9d63a2f
Lucas <lucasgoncalvesb@gmail.com>
2019-06-09 17:31:51 -0300

    Work done...

--- README.md
+++ README.md
@@ -2,17 +2,4 @@
 My dotfiles
 
 ## Install
-With selected options:
-`wget -q - https://git.io/fjzw6 | bash -s {parameters}`
-
-Full install:
-`wget -q - https://git.io/fjzw6 | bash -s -a`
-
-### Parameters (not yet implemented...)
- - -a ALL tools!!
- - -c Command line tools
- - -m Miscelaneous GUI tools
- - -j Java tools
- - -p Python tools
- - -n NodeJS tools
- - -g Golang tools
+`wget -q - https://git.io/fjzw6
---
+++ git-add-key.sh
@@ -0,0 +1,10 @@
+mv ~/.ssh/id_rsa ~/.ssh/id_rsa_bak 
+mv ~/.ssh/id_rsa.pub ~/.ssh/id_rsa.pub_bak
+
+ssh-keygen -t rsa
+ssh-add ~/.ssh/id_rsa
+pub=`cat ~/.ssh/id_rsa.pub`
+read -p "Enter github username: " githubuser
+echo "Using username $githubuser"
+read -s -p "Enter github password for user $githubuser: " githubpass
+curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
