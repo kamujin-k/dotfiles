2a29bf270957a78bc54c63169ce6fe57ace9c651
Lucas <lucasgoncalvesb@gmail.com>
2019-06-30 18:43:13 -0300

    blabla

--- git-add-key.sh
+++ git-add-key.sh
@@ -6,5 +6,5 @@ ssh-add ~/.ssh/id_rsa
 pub=`cat ~/.ssh/id_rsa.pub`
 read -p "Enter github username: " githubuser
 echo "Using username $githubuser"
-read -s -p "Enter github password for user $githubuser: " githubpass
+read -p "Enter github password for user $githubuser: " githubpass
 curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
