22eeabbdee866fbd5faebbb961b3dae28d5ea4b0
Lucas <lucasgoncalvesb@gmail.com>
2019-07-03 12:04:29 -0300

    Improvements on ssh key generation for Github

--- git-add-key.sh
+++ git-add-key.sh
@@ -1,13 +1,16 @@
-mv  ~/.ssh/id_rsa  ~/.ssh/id_rsa_bak 
-mv  ~/.ssh/id_rsa.pub  ~/.ssh/id_rsa.pub_bak
-
-ssh-keygen -t rsa
-ssh-add ~/.ssh/id_rsa
-pub=`cat ~/.ssh/id_rsa.pub`
-read -p "Enter github username: " githubuser
-echo "Using username $githubuser"
-read -p "Enter github password for user $githubuser: " githubpass
-curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
 
 
+if [ ! -e ~/.ssh/id_rsa_github ]; then
+    # ssh-keygen -t rsa
+    ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa_github -q -N ""
+    ssh-add ~/.ssh/id_rsa_github
 
+    pub=`cat ~/.ssh/id_rsa_github.pub`
+
+    read -p "Enter github username: " githubuser
+    echo "Using username $githubuser"
+
+    read -s -p "Enter github password for user $githubuser: " githubpass
+
+    curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
+fi
